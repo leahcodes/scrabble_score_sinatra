13a002950ac912d4e5a74a77aa27d504afbb00df
Alexa Zeazas & Clayton Easterbrooks <student@epicodus.com>
2015-08-04 11:06:07 -0700

    Add first spec.

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -2,6 +2,7 @@ require('rspec')
 require('scrabble')
 
 describe('String#scrabble') do
-  # put specs here
+  it('returns a scrabble score for a letter') do
+    expect("a".scrabble()).to(eq(1))
+  end
 end
-  
