36d12276a5633678087816118799858e901802eb
Alexa Zeazas & Clayton Easterbrooks <student@epicodus.com>
2015-08-04 11:56:43 -0700

    Write second spec

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -5,4 +5,12 @@ describe('String#scrabble') do
   it('returns a scrabble score for a letter') do
     expect("a".scrabble()).to(eq(1))
   end
+
+  it('Returns a scrabble score for a word') do
+  #Code needs to split word into array of letters, then iterate in each() loop
+  #through the letter, keeping a running tally of the Scrabble score values of the
+  #letters
+    expect("Word".scrabble()).to(eq(8))
+  end
+  
 end
