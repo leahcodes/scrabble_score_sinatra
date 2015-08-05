8b5637242ecf9935f6cf6456af82538a2a4793f4
Clayton Easterbrooks & Leah Klamkin <student@epicodus.com>
2015-08-05 11:13:34 -0700

    wrote index.erb file

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/scrabble.rb')
+also_reload('lib/**/*.rb')
+
 
 get('/') do
   erb(:index)
+end
+
+get('/score') do
+  @score = params.fetch('word').scrabble()
+  erb(:score)
 end
