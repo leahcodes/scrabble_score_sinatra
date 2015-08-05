2c0c0137779d537f1af3e1d252b0640740dac29f
Clayton Easterbrooks & Leah Klamkin <student@epicodus.com>
2015-08-05 11:32:15 -0700

    Final version.

--- spec/scrabble_integration_spec.rb
+++ spec/scrabble_integration_spec.rb
@@ -6,7 +6,7 @@ describe('the scrabble score path', {:type => :feature}) do
   it('processes the user entry and returns the scrabble score') do
     visit('/')
     fill_in('score', :with => 'word')
-    click_button('Perform calculation')
+    click_button('Perform Calculation')
     expect(page).to have_content('8')
   end
 end
